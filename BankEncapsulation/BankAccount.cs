8673208be5abf7fcc9eb9af85c6053a008d984ea
Travis Gersch <t_gersch@hotmail.com>
2022-07-14 16:07:52 -0500

    bank encapsulation initial commit

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation;
+
+public class BankAccount
+{
+    private double balance = 0;
+
+    public void Deposit(double amount)
+    {
+        balance += amount; //if the beginning balance was anyything other than zero, balance = amount wouldn't work
+    }                      // so used += instead. redundant if balance stays zero             
+
+    public double GetBalance()
+    {
+        return balance;
+    }
+}
---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,12 @@
+﻿using BankEncapsulation;
+
+BankAccount account1 = new BankAccount();
+
+Console.WriteLine("Hello! How much would you like to deposit today?\n");
+
+var depoAmmount = double.Parse(Console.ReadLine()); //Convert.ToInt32 won't allow decimals - TryParse unnecessary
+                                                       //if not going to loop for a continuous interactive deposit simulation
+
+account1.Deposit(depoAmmount);
+
+Console.WriteLine($"\nDeposit amount: ${depoAmmount}\n\nNew Balance: ${account1.GetBalance()}");
